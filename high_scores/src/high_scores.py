708e4beb4e53e4baba467940f1da4c14d8052deb
Stuart-McCafferty <Stuartj678@gmail.com>
2022-06-30 14:19:45 +0100

    finished highScores and passed all tests

--- high_scores/src/high_scores.py
+++ high_scores/src/high_scores.py
@@ -1,10 +1,26 @@
+from xml.dom.minidom import parseString
+
+
 def latest(scores):
-    pass
+    return scores[-1]
 
 
 def personal_best(scores):
-    pass
+    return max(scores)
 
 
 def personal_top_three(scores):
-    pass
+    top_three = []
+    counter = 0
+    if len(scores) <= 2:
+        scores.sort(reverse = True)
+        return scores
+    while counter < 3:
+        the_max = scores.index(max(scores))
+        top_three.append(scores.pop(the_max))
+        counter += 1
+    return top_three
+
+def descending_list(scores):
+    scores.sort(reverse = True)
+    return scores
--- high_scores/tests/high_scores_test.py
+++ high_scores/tests/high_scores_test.py
@@ -1,27 +1,33 @@
 import unittest
 
-from src.high_scores import latest, personal_best, personal_top_three
+from src.high_scores import *
 
 
 class HighScoresTest(unittest.TestCase):
     
     def setUp(self):
-        self.scoreList = [5, 1, 6, 8, 9]
+        self.scoreList = [5, 1, 10, 8, 9]
     
     def test_return_latest_score(self):
-        self.assertEqual(9, self.latest(self.scoreList))
+        self.assertEqual(9, latest(self.scoreList))
 
-    # Test latest score (the last thing in the list)
+    def test_return_max_score(self):
+        self.assertEqual(10, personal_best(self.scoreList))
 
-    # Test personal best (the highest score in the list)
+    def test_return_top_three_scores(self):
+        self.assertEqual([10, 9, 8], personal_top_three(self.scoreList))
 
-    # Test top three from list of scores
+    def test_return_descending_list(self):
+        self.assertEqual([10, 9 ,8 , 5, 1], descending_list(self.scoreList))
 
-    # Test ordered from highest tp lowest
+    def test_return_three_same_top_scores(self):
+        self.tiedScores = [12, 10, 10, 6 ,10 , 7 , 8]
+        self.assertEqual([12, 10, 10], personal_top_three(self.tiedScores))
 
-    # Test top three when there is a tie
-
-    # Test top three when there are less than three
-
-    # Test top three when there is only one
-    
+    def test_return_top_three_when_less_than_three(self):
+        self.twoScores = [2, 4]
+        self.assertEqual([4, 2], personal_top_three(self.twoScores))
+        
+    def test_return_top_one_when_there_is_one(self):
+        self.oneScore = [1]
+        self.assertEqual([1], personal_top_three(self.oneScore))    
